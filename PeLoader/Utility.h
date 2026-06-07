a5a92b2d8ff7a44b12a54b0ca916721b984b70ab
Yahel105 <yahelj6@gmail.com>
2026-06-07 03:13:02 +0300

    add: RAII deleters for HANDLE and HMODULE

--- PeLoader/Utility.h
+++ PeLoader/Utility.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <Windows.h>
+#include <variant>
 
 template <typename T, typename PBase>
 T resolve_rva(PBase base, size_t offset)
@@ -9,3 +10,17 @@ T resolve_rva(PBase base, size_t offset)
 	static_assert(std::is_pointer<PBase>::value, "has to be a pointer");
 	return reinterpret_cast<T>(reinterpret_cast<PBYTE>(base) + offset);
 }
+
+struct HandleDeleter {
+	using pointer = HANDLE;
+	void operator()(HANDLE h) const { CloseHandle(h); }
+};
+using HHandle = std::unique_ptr<void, HandleDeleter>;
+
+
+struct HModuleDeleter {
+	using pointer = HMODULE;
+	void operator()(HMODULE h) const { FreeLibrary(h); }
+};
+using ModuleHandle = std::unique_ptr<void,HModuleDeleter>;
+
